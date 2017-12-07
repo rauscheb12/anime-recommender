d56dcbd8f056c1b37e4587821b7b124ef2af14d9
rauscheb12 <rauscheb12@uww.edu>
2017-12-07 15:00:07 -0600

    Updated file directories

---
+++ pdo_connect.php
@@ -0,0 +1,15 @@
+***REMOVED***
+
+$user = 'rauscheb12';
+$pass = 'er2788'; // first initial last initial last 4-digits of ID'
+$db_info='mysql:host=washington.uww.edu; dbname=cs382-2177_rauscheb12';
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED***
---
+++ view/default.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Anim.mender - Home</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+        crossorigin="anonymous">
+
+</head>
+
+<body>
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-toggleable-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Anim.mender</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="./default.html">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./addanime.html">Add an Anime</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./search.html">Advanced Search</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Page Header -->
+    <center>
+        <h3>Quick Search</h3>
+    </center>
+
+    <!-- Form -->
+    <form>
+        <!-- Title -->
+        <div class="row">
+            <div class="col"></div>
+            <div class="form-group col-10">
+                <label for="anime-title">Title</label>
+                <input type="text" class="form-control" id="anime-title" placeholder="Title of the anime (e.g. 'Naruto')">
+            </div>
+            <div class="col"></div>
+        </div>
+        <!-- Submit -->
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-10">
+                <input type="submit" class="btn btn-primary form-control" id="submit-btn" />
+            </div>
+            <div class="col"></div>
+        </div>
+    </form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
