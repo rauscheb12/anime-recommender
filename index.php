15a31e28025d8f62a13623fdc33dea529524d8c5
rauscheb12 <rauscheb12@uww.edu>
2017-12-07 15:52:19 -0600

    Update index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+***REMOVED***
+    include('pdo_connect.php');
+    include('model/model.php');
+
+    $mode = '';
+    if(isset($_REQUEST['mode']))
+        $mode = $_REQUEST['mode'];
+
+    switch ($mode) {
+        case 'addanime' :
+            include('views/addanime.html');
+            break;
+        case 'editanime' :
+            include('views/editanime.html');
+            break;
+        case 'results' :
+            include('views/results.html');
+            break;
+        case 'search' :
+            include('views/search.html');
+            break;
+        default :
+            include('views/default.html');
+            break;
+
+    ***REMOVED***
+***REMOVED***
