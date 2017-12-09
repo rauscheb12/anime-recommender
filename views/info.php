3173d2efbe8e3e61e4c99664597ecaffae0b112f
rauscheb12 <rauscheb12@uww.edu>
2017-12-09 12:25:38 -0600

    Update info page

--- views/info.php
+++ views/info.php
@@ -1,38 +1,44 @@
 <?php
 
-//echo "<!-- Page Header -->";
+// page header
 echo "<center><h3>";
-if (isset($table_data)){
-    echo $table_data[1];
+if (isset($table_data) && $table_data['anime_id'] != 0){
+    echo $table_data['name'];
 }else{
     echo "No Anime Found.";
 }
 echo "</h3></center>";
-echo "<div class='row'>";
-echo "<div class='col'></div>";
-echo "<div class='col-10' id='results-table'>";
-echo "<table class='table table-striped table-hover'>";
 
-if(isset($labels)&& count($labels)>0){
-    echo "<thead><tr>";
-    for ($i=0; $i<count($labels); $i++){
-        echo "<th>{$labels[$i]}</th>";
-    }
-    echo "</tr></thead>";
-}
+// fill table
+if (isset($table_data) && $table_data['anime_id'] != 0){
 
-if (isset($table_data)){
-    echo "<tbody>";
-    for ($i=0; $i<count($table_data); $i++){
-        $row = $table_data[$i];
-        echo "<tr>";
-        foreach ($row as $key=>$value){
-            echo "<td> {$value} </td>";
+    echo "<div class='row'>";
+    echo "<div class='col'></div>";
+    echo "<div class='col-10' id='results-table'>";
+    echo "<table class='table table-striped table-hover'>";
+
+    if(isset($labels)&& count($labels)>0){
+        echo "<thead><tr>";
+        for ($i=0; $i<count($labels); $i++){
+            echo "<th>{$labels[$i]}</th>";
         }
-        echo "</tr>";
+        echo "</tr></thead>";
     }
+
+    echo "<tbody>";
+    echo "<td>{$table_data['anime_id']}</td>";
+    echo "<td>{$table_data['name']}</td>";
+    echo "<td>{$table_data['type']}</td>";
+    echo "<td>{$table_data['episodes']}</td>";
+    echo "<td>{$table_data['score']}</td>";
+    echo "<td>{$table_data['members']}</td>";
+    echo "<td>{$table_data['studio_id']}</td>";
+    echo "<td>{$table_data['rating']}</td>";
+    echo "<td>{$table_data['status']}</td>";
+    echo "<td>{$table_data['premiered']}</td>";
     echo "</tbody>";
 }
+
 
 echo "</table>";
 echo "</div>";
