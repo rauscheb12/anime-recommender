ba7703366f50b517edd6c5bcdceb1084d3c41f54
rauscheb12 <rauscheb12@uww.edu>
2017-12-09 11:41:17 -0600

    Created info page

---
+++ views/info.php
@@ -0,0 +1,42 @@
+***REMOVED***
+
+//echo "<!-- Page Header -->";
+echo "<center><h3>";
+if (isset($table_data)){
+    echo $table_data[1];
+***REMOVED***else{
+    echo "No Anime Found.";
+***REMOVED***
+echo "</h3></center>";
+echo "<div class='row'>";
+echo "<div class='col'></div>";
+echo "<div class='col-10' id='results-table'>";
+echo "<table class='table table-striped table-hover'>";
+
+if(isset($labels)&& count($labels)>0){
+    echo "<thead><tr>";
+    for ($i=0; $i<count($labels); $i++){
+        echo "<th>{$labels[$i]***REMOVED***</th>";
+    ***REMOVED***
+    echo "</tr></thead>";
+***REMOVED***
+
+if (isset($table_data)){
+    echo "<tbody>";
+    for ($i=0; $i<count($table_data); $i++){
+        $row = $table_data[$i];
+        echo "<tr>";
+        foreach ($row as $key=>$value){
+            echo "<td> {$value***REMOVED*** </td>";
+        ***REMOVED***
+        echo "</tr>";
+    ***REMOVED***
+    echo "</tbody>";
+***REMOVED***
+
+echo "</table>";
+echo "</div>";
+echo "<div class='col'></div>";
+echo "</div>";
+
+***REMOVED***
